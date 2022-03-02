51a6f807ae68b52fe44c5df0ca3a56ff82d8e6a7
yulad <yulad@MATEBOOK>
2022-03-02 22:04:05 +0700

    Добавьте файлы проекта.

---
+++ Wpf6/WeatherControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+
+namespace Wpf6
+{
+    enum Precipitation
+    {
+        sunny,
+        cloudy,
+        rain,
+        snow
+    }
+    internal class WeatherControl : DependencyObject
+    {
+        private Precipitation precipitation;
+        private string wind_direction;
+        private int wind_speed;
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+        public WeatherControl(string winddir,int windsp, Precipitation precipitation)
+        {
+            WindDirection = winddir;
+            WindSpeed = windsp;
+            this.precipitation = precipitation;
+        }
+        public static readonly DependencyProperty TempProperty;
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+            set => SetValue(TempProperty, value);
+        }
+        static WeatherControl()
+        {
+            TempProperty = DependencyProperty.Register(
+                nameof(Temp),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemp)),
+                new ValidateValueCallback(ValidateTemp));
+        }
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if ( v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
